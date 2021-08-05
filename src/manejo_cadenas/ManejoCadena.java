bdd2bc633ca3e42548d38e211a534b5c3801ddd4
Manuel Rojas <manuerjs70@gmail.com>
2021-08-04 18:01:40 -0600

    Agregue manejador de cadenas

---
+++ src/manejo_cadenas/ManejoCadena.java
@@ -0,0 +1,40 @@
+package src.manejo_cadenas;
+
+public class ManejoCadena {
+
+    private static final char[] SIMBOLOS = {'{', '}', '(', ')', '[', ']', ';', ','};
+
+    public static boolean esEntero(String text){
+        boolean valido = false;
+        try {
+            Integer.parseInt(text);
+            valido = true;
+
+        } catch (NumberFormatException e) {
+            System.out.println("No es un entero");
+        }
+        return valido;
+    }
+
+    public static boolean esDecimal(String text){
+        boolean valido = false;
+        try {
+            Double.parseDouble(text);
+            valido = true;
+        } catch (NumberFormatException e) {
+            System.out.println("No es decimal");
+        }
+        return valido;
+    }
+
+    public static boolean esSimbolo(char simb){
+        boolean igual = false;
+        for (int i = 0; i < SIMBOLOS.length; i++) {
+            if (simb == SIMBOLOS[i]) {
+                igual = true;
+            }
+        }
+        return igual;
+    }
+    
+}
