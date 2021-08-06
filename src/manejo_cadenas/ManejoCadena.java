1002637460e5ea8325761a88c7b84a38608f1e50
Manuel Rojas <manuerjs70@gmail.com>
2021-08-05 18:42:26 -0600

    Agregue metodos para reconocer identificadores

--- src/manejo_cadenas/ManejoCadena.java
+++ src/manejo_cadenas/ManejoCadena.java
@@ -37,10 +37,30 @@ public class ManejoCadena {
         return igual;
     }
 
+    public static boolean esId(String text){
+        text = text.toUpperCase();
+        boolean valido = false;
+        if (text.length() == 0) {
+            return false;
+        }
+        for (int i = 65; i <= 90; i++) {
+            char compara = (char) i;
+            if (text.charAt(0) == compara) {
+                valido = true;
+            }
+        }
+        return valido;
+    }
+
     public static String quitarSalto(String text){
         text = text.trim();
         text = text.replace("\n", " ");
         return text;
     }
     
+    public static String[] listaPalabras(String text){
+        text = quitarSalto(text);
+        String[] palabras = text.split(" ");
+        return palabras;
+    }
 }
