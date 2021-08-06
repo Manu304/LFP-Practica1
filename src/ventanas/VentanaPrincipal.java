79f6a45207c2d620f610fab180d7a0d06a73b134
Manuel Rojas <manuerjs70@gmail.com>
2021-08-05 20:53:27 -0600

    Agregue funcionalidad a ventana principal

--- src/ventanas/VentanaPrincipal.java
+++ src/ventanas/VentanaPrincipal.java
@@ -1,15 +1,64 @@
 package src.ventanas;
 
+import java.util.ArrayList;
+import javax.swing.DefaultListModel;
+import javax.swing.JOptionPane;
+import src.manejo_cadenas.ManejadorToken;
+import src.token.TipoToken;
+import src.token.Token;
+
 /**
  *
  * @author Manuel Rojas
  */
 public class VentanaPrincipal extends javax.swing.JFrame {
 
+    private DefaultListModel modeloId = new DefaultListModel();
+    private DefaultListModel modeloEntero = new DefaultListModel();
+    private DefaultListModel modeloDecimal = new DefaultListModel();
+    private DefaultListModel modeloSimbolo = new DefaultListModel();
+    private DefaultListModel modeloError = new DefaultListModel();
+
     public VentanaPrincipal() {
         initComponents();
         setVisible(true);
         setLocationRelativeTo(null);
+    }
+
+    private void listarElementos(ArrayList<Token> tokens) {
+        for (int i = 0; i < tokens.size(); i++) {
+            switch (tokens.get(i).getTipo()) {
+                case IDENTIFICADOR -> {
+                    modeloId.addElement(tokens.get(i));
+                }
+                case NUMERO_ENTERO -> {
+                    modeloEntero.addElement(tokens.get(i));
+                }
+                case NUMERO_DECIMAL -> {
+                    modeloDecimal.addElement(tokens.get(i));
+                }
+                case SIMBOLO -> {
+                    modeloSimbolo.addElement(tokens.get(i));
+                }
+                case ERROR -> {
+                    modeloError.addElement(tokens.get(i));
+                }
+            }
+
+        }
+        jListId.setModel(modeloId);
+        jListEntero.setModel(modeloEntero);
+        jListDecimal.setModel(modeloDecimal);
+        jListSimbolo.setModel(modeloSimbolo);
+        jListError.setModel(modeloError);
+    }
+
+    private void limpiarModelos() {
+        modeloId.removeAllElements();
+        modeloEntero.removeAllElements();
+        modeloDecimal.removeAllElements();
+        modeloSimbolo.removeAllElements();
+        modeloError.removeAllElements();
     }
 
     @SuppressWarnings("unchecked")
@@ -27,22 +76,24 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         labelError = new javax.swing.JLabel();
         jPanel5 = new javax.swing.JPanel();
         jScrollPane3 = new javax.swing.JScrollPane();
-        jList1 = new javax.swing.JList<>();
+        jListId = new javax.swing.JList<>();
         jScrollPane4 = new javax.swing.JScrollPane();
-        jList2 = new javax.swing.JList<>();
+        jListEntero = new javax.swing.JList<>();
         jScrollPane5 = new javax.swing.JScrollPane();
-        jList3 = new javax.swing.JList<>();
+        jListDecimal = new javax.swing.JList<>();
         jScrollPane6 = new javax.swing.JScrollPane();
-        jList4 = new javax.swing.JList<>();
+        jListSimbolo = new javax.swing.JList<>();
         jScrollPane7 = new javax.swing.JScrollPane();
-        jList5 = new javax.swing.JList<>();
+        jListError = new javax.swing.JList<>();
         jPanel3 = new javax.swing.JPanel();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTextArea2 = new javax.swing.JTextArea();
-        jButton1 = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        botonValidar = new javax.swing.JButton();
+        botonLimpiar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(1024, 400));
+        setMinimumSize(new java.awt.Dimension(1024, 400));
 
         jPanel1.setLayout(new java.awt.BorderLayout());
 
@@ -51,8 +102,8 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         jLabel1.setText("VALIDADOR DE TOKENS");
         jPanel1.add(jLabel1, java.awt.BorderLayout.NORTH);
 
-        panelListas.setMinimumSize(new java.awt.Dimension(500, 161));
-        panelListas.setPreferredSize(new java.awt.Dimension(500, 161));
+        panelListas.setMinimumSize(new java.awt.Dimension(500, 200));
+        panelListas.setPreferredSize(new java.awt.Dimension(500, 200));
         panelListas.setLayout(new java.awt.BorderLayout());
 
         jPanel6.setLayout(new java.awt.GridLayout(1, 0));
@@ -86,53 +137,28 @@ public class VentanaPrincipal extends javax.swing.JFrame {
 
         jPanel5.setLayout(new java.awt.GridLayout(1, 0));
 
-        jList1.setFont(new java.awt.Font("Poppins ExtraLight", 0, 12)); // NOI18N
-        jList1.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
-        jScrollPane3.setViewportView(jList1);
+        jListId.setFont(new java.awt.Font("Poppins ExtraLight", 0, 12)); // NOI18N
+        jScrollPane3.setViewportView(jListId);
 
         jPanel5.add(jScrollPane3);
 
-        jList2.setFont(new java.awt.Font("Poppins ExtraLight", 0, 12)); // NOI18N
-        jList2.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
-        jScrollPane4.setViewportView(jList2);
+        jListEntero.setFont(new java.awt.Font("Poppins ExtraLight", 0, 12)); // NOI18N
+        jScrollPane4.setViewportView(jListEntero);
 
         jPanel5.add(jScrollPane4);
 
-        jList3.setFont(new java.awt.Font("Poppins ExtraLight", 0, 12)); // NOI18N
-        jList3.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
-        jScrollPane5.setViewportView(jList3);
+        jListDecimal.setFont(new java.awt.Font("Poppins ExtraLight", 0, 12)); // NOI18N
+        jScrollPane5.setViewportView(jListDecimal);
 
         jPanel5.add(jScrollPane5);
 
-        jList4.setFont(new java.awt.Font("Poppins ExtraLight", 0, 12)); // NOI18N
-        jList4.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
-        jScrollPane6.setViewportView(jList4);
+        jListSimbolo.setFont(new java.awt.Font("Poppins ExtraLight", 0, 12)); // NOI18N
+        jScrollPane6.setViewportView(jListSimbolo);
 
         jPanel5.add(jScrollPane6);
 
-        jList5.setFont(new java.awt.Font("Poppins ExtraLight", 0, 12)); // NOI18N
-        jList5.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
-        jScrollPane7.setViewportView(jList5);
+        jListError.setFont(new java.awt.Font("Poppins ExtraLight", 0, 12)); // NOI18N
+        jScrollPane7.setViewportView(jListError);
 
         jPanel5.add(jScrollPane7);
 
@@ -149,12 +175,33 @@ public class VentanaPrincipal extends javax.swing.JFrame {
 
         jPanel3.add(jScrollPane2, java.awt.BorderLayout.CENTER);
 
-        jButton1.setFont(new java.awt.Font("Poppins", 1, 24)); // NOI18N
-        jButton1.setText("VERIFICAR");
-        jButton1.setMaximumSize(new java.awt.Dimension(100, 50));
-        jButton1.setMinimumSize(new java.awt.Dimension(100, 50));
-        jButton1.setPreferredSize(new java.awt.Dimension(100, 50));
-        jPanel3.add(jButton1, java.awt.BorderLayout.SOUTH);
+        jPanel2.setLayout(new java.awt.GridLayout(1, 0));
+
+        botonValidar.setFont(new java.awt.Font("Poppins", 1, 24)); // NOI18N
+        botonValidar.setText("VALIDAR");
+        botonValidar.setMaximumSize(new java.awt.Dimension(77, 50));
+        botonValidar.setMinimumSize(new java.awt.Dimension(77, 50));
+        botonValidar.setPreferredSize(new java.awt.Dimension(77, 50));
+        botonValidar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonValidarActionPerformed(evt);
+            }
+        });
+        jPanel2.add(botonValidar);
+
+        botonLimpiar.setFont(new java.awt.Font("Poppins", 1, 24)); // NOI18N
+        botonLimpiar.setText("LIMPIAR");
+        botonLimpiar.setMaximumSize(new java.awt.Dimension(75, 50));
+        botonLimpiar.setMinimumSize(new java.awt.Dimension(75, 50));
+        botonLimpiar.setPreferredSize(new java.awt.Dimension(75, 50));
+        botonLimpiar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonLimpiarActionPerformed(evt);
+            }
+        });
+        jPanel2.add(botonLimpiar);
+
+        jPanel3.add(jPanel2, java.awt.BorderLayout.SOUTH);
 
         jPanel1.add(jPanel3, java.awt.BorderLayout.CENTER);
 
@@ -163,15 +210,33 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void botonValidarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonValidarActionPerformed
+        limpiarModelos();
+        if (!jTextArea2.getText().isBlank()) {
+            ManejadorToken manejador = new ManejadorToken(jTextArea2.getText());
+            listarElementos(manejador.getTokens());
+        } else {
+            JOptionPane.showMessageDialog(this, "No hay texto para procesar", "No hay texto", JOptionPane.WARNING_MESSAGE);
+            jTextArea2.setText("");
+        }
+    }//GEN-LAST:event_botonValidarActionPerformed
+
+    private void botonLimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonLimpiarActionPerformed
+        limpiarModelos();
+        jTextArea2.setText("");
+    }//GEN-LAST:event_botonLimpiarActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton botonLimpiar;
+    private javax.swing.JButton botonValidar;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JList<String> jList1;
-    private javax.swing.JList<String> jList2;
-    private javax.swing.JList<String> jList3;
-    private javax.swing.JList<String> jList4;
-    private javax.swing.JList<String> jList5;
+    private javax.swing.JList<String> jListDecimal;
+    private javax.swing.JList<String> jListEntero;
+    private javax.swing.JList<String> jListError;
+    private javax.swing.JList<String> jListId;
+    private javax.swing.JList<String> jListSimbolo;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel5;
     private javax.swing.JPanel jPanel6;
